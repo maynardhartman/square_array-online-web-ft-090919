d2314f42bd681c8d06963ebd92566fa74dcc8725
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-11 14:49:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0
   # your code here
-  array.each {|element_to_squard| 
+  array.each {|element_to_square| 
     array[counter = element_to_square * element_to_square]
     counter += 1
   }
