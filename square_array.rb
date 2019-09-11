a37f6a771c3251dcb1977fda76377cd0e9568945
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-11 14:44:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   array.each {|element_to_squard| 
   
-    element_to_square = element_to_square * element_to_square 
+    puts (element_to_square * element_to_square) 
     
   }
 end
